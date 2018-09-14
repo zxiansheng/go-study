9ace6b670cf9ca4b47880a27f13aaf5e0e56104c
zxiansheng <421605777@qq.com>
2018-09-14 19:05:22 +0800

    test

--- gotodemo.go
+++ gotodemo.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"strings"
+
+	"github.com/tidwall/gjson"
 
 	"github.com/axgle/mahonia"
 )
@@ -18,10 +22,11 @@ func test() {
 	response, _ := http.Get("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=15850781443")
 	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
-	fmt.Println("type:", reflect.TypeOf(body))
 	shows := mahonia.NewDecoder("GB18030")
 	ret := shows.ConvertString(string(body))
-	fmt.Println(ret)
+	fmt.Println("type:", reflect.TypeOf(ret))
+	strsplit(ret)
+	//	fmt.Println(ret)
 	if response.StatusCode == 200 {
 		fmt.Println("ok")
 	} else {
@@ -30,6 +35,7 @@ func test() {
 }
 
 func get() {
+	//	http://api.k780.com:88/?app=phone.get&phone=13800138000&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json
 	response, _ := http.Get("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=15850781443")
 	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
@@ -40,6 +46,27 @@ func get() {
 	} else {
 		fmt.Println("error")
 	}
+}
+
+// 解析手机号 运营商
+func strsplit(vals string) {
+	arr := strings.Split(vals, "=")
+	jsonArr := arr[1]
+
+	// other test
+	ret, _ := json.Marshal(jsonArr)
+	type Animal struct {
+		CatName string
+	}
+	var animals []Animal
+	err := json.Unmarshal(ret, &animals)
+
+	fmt.Println(1111, ret, reflect.TypeOf(ret), 222, err, 333)
+
+	jsonMobile := gjson.Get(jsonArr, "catName")
+
+	fmt.Println(333, jsonMobile.String(), 444, reflect.TypeOf(jsonMobile.String()))
+	fmt.Println(jsonArr)
 }
 
 // 编码转换
